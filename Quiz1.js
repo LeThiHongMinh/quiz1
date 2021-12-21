5a29b4fe06be30a0acf13bef472b2618b6ba56ca
LeThiHongMinh <92656735+LeThiHongMinh@users.noreply.github.com>
2021-12-21 23:17:57 +0700

    Add files via upload

--- Quiz1.js
+++ Quiz1.js
@@ -1,11 +1,6 @@
-function result() {
-  var answer = document.getElementsByClassName("container").radio.value;
-  var buttonmain = document.getElementById("startmain").innerHTML;
-  if (answer == "developer") {
-    function developer(){
-      document.getElementById("main").innerHTML.style.visibility = "hidden"
-    document.getElementById("Developer").innerHTML.style.visibility = "visible"
-    }
-    document.getElementById("starmain").addEventListener("click", developer);
-  } 
+document.getElementById("professional").addEventListener("click", displayPro);
+
+function displayPro() {
+  document.getElementById("Developer").innerHTML.style.visibility = "visible";
 }
+
